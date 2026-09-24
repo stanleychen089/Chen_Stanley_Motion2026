8f13b6b8b4c7bfbd3503d45808f23bdb27c4ab95
chensta <chensta@OA-S143-05>
2026-09-24 19:31:24 -0400

    Task 3 completed: A script that connects 1 star to another star in the order of the list and across a drawing time using drawline, for-loop, and lerp, then disappears after completion. Can not figure out a way for the lines to or appear in sequential order. Instead, the connecting stars are drawn all at the same time.

--- Chen_Stanley_Motion2026/Assets/Scripts/Controllers/Stars.cs
+++ Chen_Stanley_Motion2026/Assets/Scripts/Controllers/Stars.cs
@@ -6,18 +6,33 @@ public class Stars : MonoBehaviour
 {
     public List<Transform> starTransforms;
     public float drawingTime;
+    public float currentTime;
+
+    public Color randomColor;
 
     // Update is called once per frame
     void Update()
     {
         DrawConstellation(starTransforms);
+        randomColor = Random.ColorHSV();
     }
 
     void DrawConstellation(List<Transform> stars)
     {
-        for(int i = 0; i > stars.Count; i++)
+        currentTime += Time.deltaTime;
+        if (currentTime > drawingTime)
         {
-            Debug.DrawLine(stars[i].position, stars[i+1].position, Color.yellow, 10);
+            currentTime = 0;
         }
+        for(int i = 0; i < stars.Count -1; i++)
+        {
+
+            Vector3 startPos = stars[i].position;
+            Vector3 endPos = stars[i + 1].position;
+            Vector3 currentPosition = Vector3.Lerp(startPos, endPos, currentTime/drawingTime);
+            Debug.DrawLine(stars[i].position, currentPosition, Color.yellow, 0);
+        }
+
+        
     }
 }
